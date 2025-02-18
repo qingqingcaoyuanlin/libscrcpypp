169186b439620501ef6509551649057888ca914a
EnderTheCoder <ggameinvader@gmail.com>
2025-02-18 17:44:15 +0800

    rgb mat

--- src/frame.cpp
+++ src/frame.cpp
@@ -27,7 +27,7 @@ namespace scrcpy {
         const std::array cv_lines_size{static_cast<std::int32_t>(image->step1()), 0, 0, 0};
         SwsContext *conversion = sws_getContext(width, height, static_cast<AVPixelFormat>(raw_frame->format), width,
                                                 height,
-                                                AV_PIX_FMT_BGR24, SWS_FAST_BILINEAR, nullptr, nullptr, nullptr);
+                                                AV_PIX_FMT_RGB24, SWS_FAST_BILINEAR, nullptr, nullptr, nullptr);
         sws_scale(conversion, raw_frame->data, raw_frame->linesize, 0, height, &image->data, cv_lines_size.data());
         sws_freeContext(conversion);
         return image;

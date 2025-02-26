ce99e2392c84374557c8eb18787352a17d2a9c19
EnderTheCoder <ggameinvader@gmail.com>
2025-02-26 17:47:16 +0800

    fix format

--- src/test_async.cpp
+++ src/test_async.cpp
@@ -18,11 +18,11 @@ int main(int argc, char *argv[]) {
     cli->set_frame_consumer([](const auto &frame) {
         std::printf("pixel format:%s\n", av_get_pix_fmt_name(static_cast<AVPixelFormat>(frame->raw()->format)));
         std::printf("h:%d w:%d\n", frame->raw()->height, frame->raw()->width);
-        auto t1 = std::chrono::high_resolution_clock::now();
+        const auto t1 = std::chrono::high_resolution_clock::now();
         auto mat = frame->mat();
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-        std::printf("pix trans fmt time: %ld µs\n", time_span.count());
+        const auto t2 = std::chrono::high_resolution_clock::now();
+        const auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+        std::printf("pix trans fmt time: %lld µs\n", time_span.count());
     });
     cli->run_recv();
     return 0;

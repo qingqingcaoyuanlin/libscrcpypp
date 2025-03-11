9736e7dfb28af6e5b563be763b875485e64d7927
EnderTheCoder <ggameinvader@gmail.com>
2025-03-11 20:15:29 +0800

    abs types

--- src/control_msg.cpp
+++ src/control_msg.cpp
@@ -5,4 +5,30 @@
 #include "control_msg.hpp"
 
 namespace scrcpy {
+    auto control_msg::join_buf(const std::vector<std::byte> &buf) -> void {
+        std::copy_n(buf.data(), buf.size(), buf_it);
+        buf_it += static_cast<std::int64_t>(buf.size());
+    }
+
+    auto control_msg::init_buf() -> std::vector<std::byte> {
+        std::vector<std::byte> buf(this->buf_size());
+        this->buf_it = buf.begin();
+        return buf;
+    }
+
+    std::size_t mouse_msg::buf_size() const {
+        return 32;
+    }
+
+    std::vector<std::uint8_t> mouse_msg::serialize() {
+        std::vector<std::uint8_t> buf;
+        buf.resize(buf_size());
+        this->join_buf(msg_type.value().serialize());
+        this->join_buf(action.value().serialize());
+        this->join_buf(pointer_id.value().serialize());
+        this->join_buf(pressure.value().serialize());
+        this->join_buf(action_button.value().serialize());
+        this->join_buf(buttons.value().serialize());
+        return buf;
+    }
 }
--- src/control_msg.hpp
+++ src/control_msg.hpp
@@ -4,7 +4,11 @@
 
 #ifndef CONTROL_MSG_HPP
 #define CONTROL_MSG_HPP
+#include <bitset>
 #include <cstdint>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 namespace scrcpy {
     namespace pointer_id {
@@ -12,6 +16,22 @@ namespace scrcpy {
         constexpr std::uint64_t GENERIC_FINGER = -2;
         constexpr std::uint64_t VIRTUAL_FINGER = -3;
     }
+
+    enum class android_keyevent_action {
+        /** The key has been pressed down. */
+        AKEY_EVENT_ACTION_DOWN = 0,
+
+        /** The key has been released. */
+        AKEY_EVENT_ACTION_UP = 1,
+
+        /**
+         * Multiple duplicate key events have occurred in a row, or a
+         * complex string is being delivered.  The repeat_count property
+         * of the key event contains the number of times the given key
+         * code should be executed.
+         */
+        AKEY_EVENT_ACTION_MULTIPLE = 2
+    };
 
     enum class control_msg_type {
         SC_CONTROL_MSG_TYPE_INJECT_KEYCODE,
@@ -40,7 +60,142 @@ namespace scrcpy {
         SC_COPY_KEY_CUT,
     };
 
+    enum class android_motionevent_buttons {
+        /** primary */
+        AMOTION_EVENT_BUTTON_PRIMARY = 1 << 0,
+        /** secondary */
+        AMOTION_EVENT_BUTTON_SECONDARY = 1 << 1,
+        /** tertiary */
+        AMOTION_EVENT_BUTTON_TERTIARY = 1 << 2,
+        /** back */
+        AMOTION_EVENT_BUTTON_BACK = 1 << 3,
+        /** forward */
+        AMOTION_EVENT_BUTTON_FORWARD = 1 << 4,
+        AMOTION_EVENT_BUTTON_STYLUS_PRIMARY = 1 << 5,
+        AMOTION_EVENT_BUTTON_STYLUS_SECONDARY = 1 << 6,
+    };
+
+    class dtype {
+    public:
+        virtual ~dtype() = default;
+
+        /**
+         * data -> bytes
+         * @return byte sequence
+         */
+        virtual auto serialize() -> std::vector<std::byte> = 0;
+    };
+
+    class sizable {
+    public:
+        virtual ~sizable() = default;
+
+        virtual auto size() -> std::size_t = 0;
+    };
+
+    class fixed_dtype : public sizable {
+    };
+
+
+    template<typename INT_TYPE, std::size_t BYTE_SIZE = sizeof(INT_TYPE)>
+    class abs_int_t : public dtype, public fixed_dtype {
+        static_assert(std::is_integral_v<INT_TYPE>);
+
+    public:
+        explicit abs_int_t(const INT_TYPE value) : value(value) {
+        }
+
+        explicit operator INT_TYPE() const {
+            return value;
+        }
+
+        auto serialize() -> std::vector<std::byte> override {
+            auto arr_ptr = reinterpret_cast<std::array<std::byte, BYTE_SIZE> *>(&value);
+            std::vector<std::byte> buffer{BYTE_SIZE};
+            std::reverse_copy(arr_ptr->begin(), arr_ptr->end(), buffer.begin());
+            return buffer;
+        }
+
+        auto size() -> std::size_t override {
+            return BYTE_SIZE;
+        }
+
+        auto val() {
+            return value;
+        }
+
+    private:
+        INT_TYPE value{0};
+    };
+
+    template<typename FLOAT_TYPE, std::size_t BYTE_SIZE = sizeof(FLOAT_TYPE)>
+    class abs_float_t final : public dtype, public fixed_dtype {
+        static_assert(std::is_floating_point_v<FLOAT_TYPE>);
+
+    public:
+        explicit abs_float_t(const FLOAT_TYPE value) : value(value) {
+        }
+
+        explicit operator FLOAT_TYPE() const {
+            return value;
+        }
+
+        auto serialize() -> std::vector<std::byte> override {
+            auto arr_ptr = reinterpret_cast<std::array<std::byte, BYTE_SIZE> *>(&value);
+            std::vector<std::byte> buffer{BYTE_SIZE};
+            std::reverse_copy(arr_ptr->begin(), arr_ptr->end(), buffer.begin());
+            return buffer;
+        }
+
+        auto size() -> std::size_t override {
+            return BYTE_SIZE;
+        }
+
+        auto val() {
+            return value;
+        }
+
+    private:
+        FLOAT_TYPE value{0.0};
+    };
+
+    template<typename ENUM_TYPE>
+    class abs_enum_t final : public abs_int_t<std::uint8_t, 1> {
+    public:
+        explicit abs_enum_t(ENUM_TYPE value)
+            : abs_int_t(value) {
+        }
+    };
+
     class control_msg {
+    public:
+        virtual ~control_msg() = default;
+
+        [[nodiscard]] virtual auto buf_size() const -> std::size_t;
+
+        virtual auto serialize() -> std::vector<std::uint8_t>;
+
+        virtual auto join_buf(const std::vector<std::byte>& buf) -> void;
+
+        virtual auto init_buf() -> std::vector<std::byte>;
+
+    private:
+        std::vector<std::byte>::iterator buf_it;
+    };
+
+    class mouse_msg final : public control_msg {
+    public:
+        [[nodiscard]] auto buf_size() const -> std::size_t override;
+
+        auto serialize() -> std::vector<std::uint8_t> override;
+
+        std::optional<abs_enum_t<control_msg_type>> msg_type;
+        std::optional<abs_enum_t<android_keyevent_action>> action;
+        std::optional<abs_int_t<std::uint64_t>> pointer_id;
+        // position position;
+        std::optional<abs_float_t<float>> pressure;
+        std::optional<abs_int_t<std::int32_t>> action_button;
+        std::optional<abs_int_t<std::int32_t>> buttons;
     };
 }
 
